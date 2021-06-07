085a69db5a3e285d5422dd63b9704bc07a12c547
RAJAT MEHRA <rajat.m.5278@gmail.com>
2021-06-07 22:03:16 +0530

    removed keys

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,13 +1,14 @@
 import firebase from "firebase/app";
 import "firebase/auth"; // for authentication
 
+// put your own firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBY04OWOIdixT2vI_lSOOR9jB3wts3DrVM",
+  apiKey: "",
   authDomain: "task-tracker-cdad2.firebaseapp.com",
   projectId: "task-tracker-cdad2",
   storageBucket: "task-tracker-cdad2.appspot.com",
-  messagingSenderId: "328905720914",
-  appId: "1:328905720914:web:b531f33cee537a4f424b8b",
+  messagingSenderId: "",
+  appId: "1::web:b531f33cee537a4f424b8b",
 };
 
 firebase.initializeApp(firebaseConfig);
